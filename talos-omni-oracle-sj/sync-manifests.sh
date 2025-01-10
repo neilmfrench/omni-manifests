1e4caea96dd50f0093506e2c8c274717cb16a792
Neil French <neilmfrench2@gmail.com>
2025-01-10 14:54:29 -0800

    Testing automated manifests

---
+++ talos-omni-oracle-sj/sync-manifests.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+export KUBECONFIG=kubeconfig
+
+declare -A helm_releases=(
+  ["cilium"]="kube-system"
+  ["coredns"]="kube-system"
+)
+
+declare -A patch_path_values=(
+  ["cilium"]=".cluster.network.cni.urls[0]"
+  ["coredns"]=".cluster.extraManifests[1]"
+)
+
+declare -A patches=(
+  ["cilium"]="cilium-cni.yaml"
+  ["coredns"]="extra-manifests.yaml"
+)
+
+for release in "${!helm_releases[@]}"; do
+  namespace=${helm_releases[$release]}
+  values_file="manifests/${release}/values.yaml"
+  install_file="manifests/${release}/install_$(date +'%Y-%m-%d_%H-%M-%S').yaml"
+  export git_manifest_file="https://raw.githubusercontent.com/neilmfrench/omni-manifests/refs/heads/main/talos-omni-oracle-sj/${install_file}"
+  export path_value="${patch_path_values[$release]}"
+  patch_file="patches/${patches[$release]}"
+
+  echo "Processing Helm release: $release in namespace: $namespace"
+
+  if kubectl get helmrelease "$release" -n "$namespace" -o yaml &>/dev/null; then
+    kubectl get helmrelease "$release" -n "$namespace" -o yaml | yq eval '.spec.values' - > "$values_file"
+    kustomize build --enable-helm "manifests/${release}" > "$install_file"
+    yq 'eval(env(path_value))=env(git_manifest_file)' -i "$patch_file"
+    echo "Updated manifests for $release"
+  else
+    echo "Error: Unable to retrieve Helm release $release in namespace $namespace"
+  fi
+done
+
+
